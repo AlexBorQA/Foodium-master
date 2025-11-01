bb31266d4850451d88b26d9119906d5641111df9
Alexey <113031830+AlexBorQA@users.noreply.github.com>
2025-11-01 12:06:49 +0500

    test(uiautomator): add BlackBoxUiAutomatorTest (3+1), waits/selectors; add uiautomator dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -152,6 +152,7 @@ dependencies {
     androidTestImplementation("androidx.test:rules:1.3.0")
     androidTestImplementation("androidx.test:core-ktx:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-contrib:3.3.0")
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
     // Hilt test utilities
     androidTestImplementation(Hilt.hiltAndroid)
     androidTestImplementation("com.google.dagger:hilt-android-testing:2.31-alpha")
